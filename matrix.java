896e22e4d2f6efbbf16923d2fec4aa1f0e8d6389
suhas <suhas.v@apisero.com>
2020-12-22 11:39:55 +0530

    matrix commit1

---
+++ matrix.java
@@ -0,0 +1,41 @@
+public class matrix {
+
+	public static void main(String[] args) {
+		int matrix1[][] = {
+				{1,2,3,4},
+				{5,6,7,8},
+				{9,10,11,12}
+		};
+		
+		int matrix2[][] = {
+				{13,14},
+				{15,16},
+				{17,18},
+				{19,20}
+				
+			
+		};
+		
+		int c[][] = new int[3][2];
+		
+		
+		for(int i=0; i<3; i++){    
+			
+			for(int j=0;j<2;j++){    
+			
+				c[i][j]=0;      
+			for(int k=0;k<4;k++)      
+			{      
+			c[i][j]+=matrix1[i][k]*matrix2[k][j];      
+			}  
+			System.out.print(c[i][j]+" ");  
+			}
+			System.out.println();
+		}
+		
+	
+	
+
+	}
+
+}
