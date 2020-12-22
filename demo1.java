0b287686b084d8de3b43bdd01fad997886be4de0
suhas <suhas.v@apisero.com>
2020-12-22 10:20:05 +0530

    first commit demo1

--- demo1.java
+++ demo1.java
@@ -1,6 +1,8 @@
-public class Demo1 {
+public class demo1 {
 	public static void main(String[] args) {
-		System.out.println("hello form Git");
-		
+		System.out.println("hello form Git 1");
+		System.out.println("hello form Git 2");
+		System.out.println("hello form Git 3");
 	}
+
 	}
