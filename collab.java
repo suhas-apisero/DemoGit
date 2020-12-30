3454e3ce30bf321d1433af52db408b5926564bc9
suhas <suhas.v@apisero.com>
2020-12-30 23:30:15 +0530

    autodesk

--- collab.java
+++ collab.java
@@ -1,5 +1,6 @@
 public class collab {
 	public static void main(String[] args) {
 		System.out.println("Collaboration1");
-}
+		System.out.println("Collaboration2");
+}	
 }
--- matrix.java
+++ matrix.java
@@ -2,7 +2,7 @@ public class matrix {
 
 	public static void main(String[] args) {
 		int matrix1[][] = {
-				{0,0,0,0},
+				{1,0,0,0},
 				{0,0,0,0},
 				{0,0,0,0}
 		};
